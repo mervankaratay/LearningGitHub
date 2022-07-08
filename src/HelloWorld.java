f4dcee8c53623a232dcabe445ee1003ed826bc92
Mervan Karatay <mervankaratay@gmail.com>
2022-07-07 21:51:52 -0400

    This code is from Ugur

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        int [] array= {1,2,4,5};
+
+        for(int i=0; i <array.length; i++) {
+            System.out.println(array[i] + "");
+        }
+
+    }
+
+}
